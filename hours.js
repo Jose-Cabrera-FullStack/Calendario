bfbf23cef9b6f52c0fdaaf2e02a6cbb92c32db5f
Cabrera Jose (Atencion a Usuarios) <mda_jcabrera@lnexternos.com.ar>
2019-12-24 00:13:49 -0300

    entorno controlado

---
+++ hours.js
@@ -0,0 +1,9 @@
+// const subjectClass = document.getElementsByClassName("subject")
+
+// for (let i = 0; i < subjectClass.length; i++) {
+//     subjectClass[i].addEventListener('click', check)
+// }
+
+// function check() {
+//     console.log(`${this.value} is ${this.checked}`)
+// }
